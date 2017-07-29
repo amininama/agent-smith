591a2b3c3a03d3981b77bbefc3602de4dcf0b56f
amininama <foozoolak@gmail.com>
2017-07-29 14:38:05 +0430

    updated launcher bash file

--- bin/launcher.sh
+++ bin/launcher.sh
@@ -6,11 +6,11 @@ CONCURRENT_CLIENT_COUNT=5
 SLEEP_BETWEEN_REQUESTS=100
 AGENTS_FILE=./agents
 TARGETS_FILE=./targets
-USE_MASTER_PROXY="true"
+USE_MASTER_PROXY="false"
 SOCKS_PROXY_HOST=127.0.0.1
 SOCKS_PROXY_PORT=1234
 MYSQL_ROOT_PASSWORD=chizchizchiz
-RELOAD_PROXIES_ON_BOOT=true
+RELOAD_PROXIES_ON_BOOT=false
 BUILD_BEFOREHAND="true"
 #================
 
@@ -28,7 +28,7 @@ STARTUP_LOG='./startup.log'
 MAIN_CLASS='sha.mpoos.agentsmith.ApplicationStarter'
 MEM_MIN='128M'
 MEM_MAX='1024M'
-OPTS='-server -XX:+AggressiveOpts -Djava.net.preferIPv4Stack=true -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=5400 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dspring.profiles.active=production'
+OPTS='-server -XX:+AggressiveOpts -Djava.net.preferIPv4Stack=true -Dcom.sun.management.jmxremote -Dspring.profiles.active=production'
 if [ "$USE_MASTER_PROXY" == "true" ]; then
     OPTS=$OPTS' -DsocksProxyHost='$SOCKS_PROXY_HOST' -DsocksProxyPort='$SOCKS_PROXY_PORT
 fi
