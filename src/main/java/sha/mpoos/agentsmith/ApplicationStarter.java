894db5cfd07832ab203fc31c494ebbd10e119fed
amininama <foozoolak@gmail.com>
2017-08-06 16:40:22 +0430

    got rid of admin-panel

--- src/main/java/sha/mpoos/agentsmith/ApplicationStarter.java
+++ src/main/java/sha/mpoos/agentsmith/ApplicationStarter.java
@@ -1,12 +1,10 @@
 package sha.mpoos.agentsmith;
 
-import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableAdminServer
 @EnableScheduling
 public class ApplicationStarter {
 	public static void main(String[] args) {
